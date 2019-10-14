cb61e04304b1597ebfe826068f464a920eb3a951
Tomek Wałkuski <contact@tomekw.com>
2019-10-14 16:21:28 +0200

    Update GRAALVM_HOME

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,5 +1,5 @@
 set EDITOR nvim
-set GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ce-19.1.1/Contents/Home
+set GRAALVM_HOME /Library/Java/JavaVirtualMachines/graalvm-ce-19.2.0.1/Contents/Home
 set PATH /usr/local/opt/node@10/bin /usr/local/sbin /usr/local/bin $PATH
 
 alias dfgit="git --work-tree=$HOME/ --git-dir=$HOME/dontfiles"
