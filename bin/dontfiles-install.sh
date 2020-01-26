42a95ccaa890fa6d5fb85567a4dda779ac39d538
Tomek Wałkuski <contact@tomekw.com>
2020-01-26 21:51:44 +0100

    Install Java before other packages

--- bin/dontfiles-install.sh
+++ bin/dontfiles-install.sh
@@ -3,6 +3,9 @@
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Java is needed for other packages, like "clojure"
+brew cask install adoptopenjdk11
+
 # Install packages
 brew bundle install
 pip3 install --user pynvim
