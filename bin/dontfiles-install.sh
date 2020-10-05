17a2f803562b93575455bc406da2dbd7fc1327f4
Tomek Wałkuski <contact@tomekw.com>
2020-10-05 10:24:20 +0200

    Install tmux plugin manager

--- bin/dontfiles-install.sh
+++ bin/dontfiles-install.sh
@@ -3,6 +3,9 @@
 # Install vim-plug
 curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
+# Install tmux plugin manager
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+
 # Java is needed for other packages, like "clojure"
 brew tap adoptopenjdk/openjdk
 brew cask install adoptopenjdk14
